1da7ad10cd7738e138a400687607b4b28340a43f
Noelle <noellekpatterson@gmail.com>
2018-05-16 15:48:42 -0700

    add plotting visual elements

--- calculations/dim_hydrograph_plotter_aggregate.py
+++ calculations/dim_hydrograph_plotter_aggregate.py
@@ -13,7 +13,7 @@ from pre_processFiles.gauge_reference import gauge_reference
 np.warnings.filterwarnings('ignore')
 
 def dim_hydrograph_plotter_agg(start_date, directory_name, end_with, class_number, gauge_numbers, plot):
-    aggregate_matrix = np.zeros((366, 5))
+    aggregate_matrix = np.zeros((366, 7))
     counter = 0
     for root,dirs,files in os.walk(directory_name):
         for file in files:
@@ -54,47 +54,59 @@ def _getAggMatrix(flow_matrix):
     number_of_rows = len(flow_matrix)
     number_of_columns = len(flow_matrix[0,:])
     normalized_matrix = np.zeros((number_of_rows, number_of_columns))
-    percentiles = np.zeros((number_of_rows, 5))
+    percentiles = np.zeros((number_of_rows, 7))
 
     for row_index, row_data in enumerate(flow_matrix[:,0]):
         for column_index, column_data in enumerate(flow_matrix[row_index, :]):
             normalized_matrix[row_index,column_index] = flow_matrix[row_index,column_index]/average_annual_flow[column_index]
 
-        percentiles[row_index,0] = np.nanpercentile(normalized_matrix[row_index,:], 10)
-        percentiles[row_index,1] = np.nanpercentile(normalized_matrix[row_index,:], 25)
-        percentiles[row_index,2] = np.nanpercentile(normalized_matrix[row_index,:], 50)
-        percentiles[row_index,3] = np.nanpercentile(normalized_matrix[row_index,:], 75)
-        percentiles[row_index,4] = np.nanpercentile(normalized_matrix[row_index,:], 90)
+        percentiles[row_index,0] = np.nanmin(normalized_matrix[row_index,:])
+        percentiles[row_index,1] = np.nanpercentile(normalized_matrix[row_index,:], 10)
+        percentiles[row_index,2] = np.nanpercentile(normalized_matrix[row_index,:], 25)
+        percentiles[row_index,3] = np.nanpercentile(normalized_matrix[row_index,:], 50)
+        percentiles[row_index,4] = np.nanpercentile(normalized_matrix[row_index,:], 75)
+        percentiles[row_index,5] = np.nanpercentile(normalized_matrix[row_index,:], 90)
+        percentiles[row_index,6] = np.nanmax(normalized_matrix[row_index,:])
 
     return percentiles
 
 def _plotter(aggregate_matrix, start_date):
-    def format_func(value, tick_number):
-        julian_start_date = datetime.strptime("{}/2001".format(start_date), "%m/%d/%Y").timetuple().tm_yday
-        return int(remove_offset_from_julian_date(value, julian_start_date))
+    # def format_func(value, tick_number):
+    #     julian_start_date = datetime.strptime("{}/2001".format(start_date), "%m/%d/%Y").timetuple().tm_yday
+    #     return int(remove_offset_from_julian_date(value, julian_start_date))
     """Dimensionless Hydrograph Plotter"""
 
     fig = plt.figure('aggregate_matrix')
     ax = plt.subplot(111)
-    plt.subplots_adjust(bottom = .2)
-    ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
+    plt.subplots_adjust(bottom = .15)
+    #ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
     x = np.arange(0,366,1)
     plt.grid(which = 'major', linestyle = '-', axis = 'y')
-    perc10 = ax.plot(aggregate_matrix[:,0], color = 'navy', label = "10%")
-    perc25 = ax.plot(aggregate_matrix[:,1], color = 'blue', label = "25%")
-    perc50 = ax.plot(aggregate_matrix[:,2], color = 'red', label = "50%")
-    perc75 = ax.plot(aggregate_matrix[:,3], color = 'blue', label = "75%")
-    perc90 = ax.plot(aggregate_matrix[:,4], color = 'navy', label = "90%")
-    ax.fill_between(x, aggregate_matrix[:,0], aggregate_matrix[:,1], color = 'powderblue')
+    min_lin = ax.plot(aggregate_matrix[:,0], color = 'black', label = 'min', lw=1)
+    perc10 = ax.plot(aggregate_matrix[:,1], color = 'navy', label = "10%")
+    perc25 = ax.plot(aggregate_matrix[:,2], color = 'blue', label = "25%")
+    perc50 = ax.plot(aggregate_matrix[:,3], color = 'red', label = "50%")
+    perc75 = ax.plot(aggregate_matrix[:,4], color = 'blue', label = "75%")
+    perc90 = ax.plot(aggregate_matrix[:,5], color = 'navy', label = "90%")
+    max_lin = ax.plot(aggregate_matrix[:,6], color = 'black', label = 'max', lw=1)
     ax.fill_between(x, aggregate_matrix[:,1], aggregate_matrix[:,2], color = 'powderblue')
     ax.fill_between(x, aggregate_matrix[:,2], aggregate_matrix[:,3], color = 'powderblue')
     ax.fill_between(x, aggregate_matrix[:,3], aggregate_matrix[:,4], color = 'powderblue')
+    ax.fill_between(x, aggregate_matrix[:,4], aggregate_matrix[:,5], color = 'powderblue')
     box = ax.get_position('aggregate_matrix')
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, ncol=5, borderaxespad = 3)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, ncol=7, borderaxespad = .9, fontsize='small', labelspacing=.2, columnspacing=1, markerscale=.5)
 
-    plt.title("Dimensionless Reference Hydrograph")
-    plt.xlabel("Julian Date")
+    plt.title("High Elevation Low Precipitation")
+    #plt.xlabel("Julian Date")
     plt.ylabel("Daily Flow/Average Annual Flow")
+
+    ax = plt.gca()
+    tick_spacing = [0, 30.5, 61, 91.5, 122, 152.5, 183, 213.5, 244, 274.5, 305, 335.5]
+    tick_labels = ['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+    plt.xticks(tick_spacing, tick_labels)
+    #ax.set_xticks(tick_spacing)
+    #tick_labels = label_xaxis[tick_spacing]
+    #ax.set_xticklabels(tick_labels)
 
     plt.grid(which = 'major', linestyle = '-', axis = 'y')
 
